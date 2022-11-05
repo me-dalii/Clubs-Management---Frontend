0fbdf36f350ea7619b26c69011d832eb216f4f64
me-dalii <medali7005@hotmail.fr>
2022-11-05 12:29:56 +0100

    jaw

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,20 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
 import { LayoutService } from 'src/app/layout/service/app.layout.service';
+import { Club } from 'src/app/models/Club';
 import { AuthenticationService } from 'src/services/authentication.service';
+import { ClubService } from 'src/services/club.service';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.scss']
+  styleUrls: ['./landing.component.scss'],
+  providers: [MessageService]
 })
 export class LandingComponent implements OnInit {
   profileItems: any[];
 
+  clubs : Club[];
+
   constructor(public layoutService: LayoutService, public router: Router, 
-    public authenticationService: AuthenticationService,) { }
+    public authenticationService: AuthenticationService, private messageService: MessageService, private clubService : ClubService,private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
+
+    this.getClubs();
 
     this.profileItems = [
       {label: 'Dashboard', icon: 'pi pi-th-large', command: () => {
@@ -40,5 +49,20 @@ export class LandingComponent implements OnInit {
   
   register() {
     this.router.navigate(['/register']);
+  }
+
+  getClubs() {
+    this.clubService.getClubs().subscribe({
+      next: (response: Club[]) => this.clubs = response,
+      error: (e) => this.messageService.add({ severity: 'error', summary: 'Erreur', detail: 'Chargement échoué', life: 3000 }),
+      complete: () => console.log(this.clubs)
+    })
+  }
+
+  getSrcFromCustomFile(club : Club) {
+  
+    let uint8Array = new Uint8Array(atob(club.logo.data).split("").map(char => char.charCodeAt(0)));
+    let dwn = new Blob([uint8Array])
+    return this.sanitizer.bypassSecurityTrustResourceUrl(URL.createObjectURL(dwn));
   }
 }
